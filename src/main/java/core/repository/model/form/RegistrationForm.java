335c96406c0ffb831f09c5642b05db02028d3156
AdrianFall <adrianq92@hotmail.com>
2016-03-30 18:35:01 +0100

    following model forms added : registration, request reset password, reset password & resend email

---
+++ src/main/java/core/repository/model/form/RegistrationForm.java
@@ -0,0 +1,50 @@
+package core.repository.model.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Adrian on 10/05/2015.
+ */
+/*@PasswordMatches()*/
+public class RegistrationForm {
+
+    @NotNull
+    @Pattern(regexp = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$")
+    private String email;
+
+    @Size(min = 5, max = 120, message = "{validation.message.Size.password}")
+    @NotNull
+    private String password;
+
+    @Size(min = 5, max = 120)
+    @NotNull
+    private String confirmPassword;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+}
---
+++ src/main/java/core/repository/model/form/ResendEmailForm.java
@@ -0,0 +1,19 @@
+package core.repository.model.form;
+
+import javax.validation.constraints.Pattern;
+
+/**
+ * Created by Adrian on 28/06/2015.
+ */
+public class ResendEmailForm {
+    @Pattern(regexp = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$", message = "Please enter a valid email format.")
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
