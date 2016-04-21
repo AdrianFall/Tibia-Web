ac374173fcbcac3a3cab5a79c99df64e18858931
AdrianFall <adrianq92@hotmail.com>
2016-04-21 19:29:01 +0100

    + get all online players

--- src/main/java/core/controller/rest/CrawlerController.java
+++ src/main/java/core/controller/rest/CrawlerController.java
@@ -1,11 +1,18 @@
 package core.controller.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.repository.model.crawler.servers.oldera.OlderaPlayer;
 import core.repository.service.CrawlerService;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Adrian on 17/04/2016.
@@ -17,8 +24,33 @@ public class CrawlerController {
     CrawlerService crawlerService;
 
     @RequestMapping(value = "/oldera/onlinePlayers", method = RequestMethod.GET)
-    public String olderaOnlinePlayers() {
-        return JSONArray.toJSONString(crawlerService.getOnlineOlderaPlayers());
+    public @ResponseBody String olderaOnlinePlayers() {
+
+        JSONArray jsonOnlinePlayers = new JSONArray();
+
+        List<OlderaPlayer> onlineOlderaplayers = crawlerService.getOnlineOlderaPlayers();
+        onlineOlderaplayers.forEach(e -> {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", e.getName());
+            jsonObject.put("level", e.getLevel());
+            jsonObject.put("vocation", e.getVocation());
+            jsonOnlinePlayers.add(jsonObject);
+        });
+
+
+
+
+
+        /*
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = null;
+        try {
+            jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(crawlerService.getOnlineOlderaPlayers());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }*/
+
+        return jsonOnlinePlayers.toJSONString();
 
     }
 }
--- src/main/java/core/repository/impl/hibernate/OlderaCrawlerRepoImpl.java
+++ src/main/java/core/repository/impl/hibernate/OlderaCrawlerRepoImpl.java
@@ -4,12 +4,15 @@ import core.repository.OlderaCrawlerRepo;
 import core.repository.model.crawler.servers.oldera.OlderaPlayer;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,10 +28,31 @@ public class OlderaCrawlerRepoImpl implements OlderaCrawlerRepo {
 
     @Override
     public List<OlderaPlayer> getOnlinePlayers() {
-        List<OlderaPlayer> onlinePlayers = (List<OlderaPlayer>) sessionFactory.getCurrentSession()
+        ProjectionList p = Projections.projectionList();
+        p.add(Projections.property("name"));
+        p.add(Projections.property("level"));
+        p.add(Projections.property("vocation"));
+        // p.add(Projections.property("id"));
+        //p.add(Projections.property("server_name"));
+        //p.add(Projections.property("server_name"));
+
+        List<Object[]> onlinePlayersArray = (List<Object[]>) sessionFactory.getCurrentSession()
                 .createCriteria(OlderaPlayer.class)
                 .add(Restrictions.eq("isOnline", true))
+                //.add(Restrictions.eq("server", "Oldera"))
+                .setProjection(Projections.distinct(p))
                 .list();
+
+        List<OlderaPlayer> onlinePlayers = new ArrayList<>();
+
+        onlinePlayersArray.forEach(e -> {
+            OlderaPlayer olderaPlayer = new OlderaPlayer();
+            olderaPlayer.setName((String) e[0]);
+            olderaPlayer.setLevel((Integer) e[1]);
+            olderaPlayer.setVocation((String) e[2]);
+            onlinePlayers.add(olderaPlayer);
+        });
+
 
         return onlinePlayers;
     }
